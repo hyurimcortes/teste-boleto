23eb1e23af18a7c110f7df7ef8eee638e1e05d58
Hyuri <hyuri.miranda.cortes@gmail.com>
2021-11-10 10:40:48 -0300

    Add boelto

--- src/app/useCases/boleto/consultaBoleto/ConsultaBoletoUseCase.ts
+++ src/app/useCases/boleto/consultaBoleto/ConsultaBoletoUseCase.ts
@@ -46,9 +46,9 @@ class ConsultaBoletoUseCase {
         
 
         return {
-            barCode:21299758700000020000001121100012100447561740,
-            amount:'20.00',
-            expirationDate:'2018-07-16'
+            barCode:barCode,
+            amount:amount,
+            expirationDate:expirationDate
         }
 
     
