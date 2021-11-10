9a00eacf422f10e55c5116396c47f28950c1c933
Hyuri <hyuri.miranda.cortes@gmail.com>
2021-11-10 10:28:12 -0300

    Rm auth

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,8 @@
 
 import { Router } from 'express';
-import { authRoutes } from './auth.routes';
 import { boletoRoutes } from './boleto.routes';
 
 const router = Router();
 
 router.use("/api/boletos", boletoRoutes);
-router.use("/api/login", authRoutes);
 export { router }
